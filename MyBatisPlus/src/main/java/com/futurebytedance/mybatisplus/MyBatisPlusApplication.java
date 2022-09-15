bc2defeb7c6b7cf70a2f9da14c8f3fdd7c700367
sssv587 <1092663295@qq.com>
2022-09-16 01:02:41 +0800

    创建mapper接口并扫描

--- MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/MyBatisPlusApplication.java
+++ MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/MyBatisPlusApplication.java
@@ -1,9 +1,12 @@
 package com.futurebytedance.mybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//扫描mapper接口所在的包
+@MapperScan("com/futurebytedance/mybatisplus/mapper/UserMapper.java")
 public class MyBatisPlusApplication {
 
     public static void main(String[] args) {
---
+++ MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.futurebytedance.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.futurebytedance.mybatisplus.pojo.User;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/16 - 0:59
+ * @Description
+ */
+public interface UserMapper extends BaseMapper<User> {
+}

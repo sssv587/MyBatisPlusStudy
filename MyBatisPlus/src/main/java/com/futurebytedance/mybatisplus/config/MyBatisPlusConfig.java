7590b931202bbd692a6b656cf541f1fdf47c8da6
sssv587 <1092663295@qq.com>
2022-09-17 00:01:36 +0800

    分页插件的配置和使用

---
+++ MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/config/MyBatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.futurebytedance.mybatisplus.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/16 - 23:52
+ * @Description 配置类
+ */
+@Configuration
+//扫描mapper接口所在的包
+@MapperScan("com.futurebytedance.mybatisplus.mapper")
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ MyBatisPlus/src/test/java/com/futurebytedance/mybatisplus/MyBatisPlusPluginsTest.java
@@ -0,0 +1,27 @@
+package com.futurebytedance.mybatisplus;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.futurebytedance.mybatisplus.mapper.UserMapper;
+import com.futurebytedance.mybatisplus.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/16 - 23:55
+ * @Description
+ */
+@SpringBootTest
+public class MyBatisPlusPluginsTest {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testPage() {
+        Page<User> page = new Page<>(2, 3);
+        userMapper.selectPage(page, null);
+        System.out.println(page);
+    }
+}

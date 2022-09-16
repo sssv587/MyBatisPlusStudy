afe420283a5d51e91dd07da98b120c41740a41d4
sssv587 <1092663295@qq.com>
2022-09-16 15:53:48 +0800

    通用Service接口

---
+++ MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/service/UserService.java
@@ -0,0 +1,14 @@
+package com.futurebytedance.mybatisplus.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.futurebytedance.mybatisplus.pojo.User;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/16 - 15:51
+ * @Description
+ */
+public interface UserService extends IService<User> {
+}
---
+++ MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.futurebytedance.mybatisplus.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.futurebytedance.mybatisplus.mapper.UserMapper;
+import com.futurebytedance.mybatisplus.pojo.User;
+import com.futurebytedance.mybatisplus.service.UserService;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2022/9/16 - 15:51
+ * @Description
+ */
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+}

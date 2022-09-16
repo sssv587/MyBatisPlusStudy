948fd890054286e63a3bff9b6791d6e1150656f0
sssv587 <1092663295@qq.com>
2022-09-17 00:37:33 +0800

    乐观锁插件

--- MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/config/MyBatisPlusConfig.java
+++ MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/config/MyBatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.futurebytedance.mybatisplus.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +22,8 @@ public class MyBatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //添加乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
     }
 }
--- MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/pojo/Product.java
+++ MyBatisPlus/src/main/java/com/futurebytedance/mybatisplus/pojo/Product.java
@@ -1,5 +1,6 @@
 package com.futurebytedance.mybatisplus.pojo;
 
+import com.baomidou.mybatisplus.annotation.Version;
 import lombok.Data;
 
 /**
@@ -13,5 +14,6 @@ public class Product {
     private Long id;
     private String name;
     private Integer price;
+    @Version //标识乐观锁版本号字段
     private Integer version;
 }

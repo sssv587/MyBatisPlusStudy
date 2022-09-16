39a6031105981ebb146a1eccbd6d8f1ec5d01240
sssv587 <1092663295@qq.com>
2022-09-16 19:28:47 +0800

    使用LambdaQueryWrapper

--- MyBatisPlus/src/test/java/com/futurebytedance/mybatisplus/MyBatisPlusWrapperTest.java
+++ MyBatisPlus/src/test/java/com/futurebytedance/mybatisplus/MyBatisPlusWrapperTest.java
@@ -1,5 +1,6 @@
 package com.futurebytedance.mybatisplus;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
@@ -156,6 +157,21 @@ public class MyBatisPlusWrapperTest {
         queryWrapper.like(StringUtils.isNotBlank(username), "name", username)
                 .ge(ageBegin != null, "age", ageBegin)
                 .le(ageEnd != null, "age", ageEnd);
+        List<User> list = userMapper.selectList(queryWrapper);
+        list.forEach(System.out::println);
+    }
+
+    //使用LambdaQueryWrapper
+    @Test
+    public void test11() {
+        //SELECT id,name,age,email,is_deleted FROM t_user WHERE is_deleted=0 AND (name LIKE ? AND age <= ?)
+        String username = "a";
+        Integer ageBegin = null;
+        Integer ageEnd = 30;
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotBlank(username), User::getName, username)
+                .ge(ageBegin != null, User::getAge, ageBegin)
+                .le(ageEnd != null, User::getAge, ageEnd);
         List<User> list = userMapper.selectList(queryWrapper);
         list.forEach(System.out::println);
     }

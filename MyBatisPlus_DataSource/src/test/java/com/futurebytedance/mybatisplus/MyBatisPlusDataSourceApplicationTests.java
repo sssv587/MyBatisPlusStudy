38f6c010ac29875ce627cfee5133cc03b099a59d
sssv587 <1092663295@qq.com>
2022-09-17 15:34:50 +0800

    测试多数据源

--- MyBatisPlus_DataSource/src/test/java/com/futurebytedance/mybatisplus/MyBatisPlusDataSourceApplicationTests.java
+++ MyBatisPlus_DataSource/src/test/java/com/futurebytedance/mybatisplus/MyBatisPlusDataSourceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.futurebytedance.mybatisplus;
 
+import com.futurebytedance.mybatisplus.service.ProductService;
+import com.futurebytedance.mybatisplus.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +11,17 @@ class MyBatisPlusDataSourceApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void test(){
+        System.out.println(userService.getById(1));
+        System.out.println(productService.getById(1));
     }
 
 }
